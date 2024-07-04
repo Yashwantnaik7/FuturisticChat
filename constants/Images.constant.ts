fd6a7e14d42a059bcaebe7586fc62bd2f3e39205
Yashwantnaik7 <yashnaik322@gmail.com>
2024-07-04 07:43:10 +0530

    required image for chat screen added

---
+++ constants/Images.constant.ts
@@ -0,0 +1,11 @@
+const Images = {
+  tabbar: {
+    chatBot: require('../assets/images/chat_bot.png'),
+  },
+  chat: {
+    sendMsg: require('../assets/images/send_msg.png'),
+    micOn: require('../assets/images/mic_on.png'),
+  },
+};
+
+export default Images;
